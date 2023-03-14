1044a0469bd2b2ef1a8e3d98e4aba83b13b38e14
GuiCamargoDev <124837745+GuiCamargoDev@users.noreply.github.com>
2023-03-14 18:08:02 -0300

    Add files via upload

---
+++ js3.js
@@ -0,0 +1,3 @@
+var nome = prompt("Digite seu nome");
+var idade = prompt("Informe sua idade");
+document.write("Olá " + nome + ", sua idade é " + idade);
---
+++ js4.js
@@ -0,0 +1,32 @@
+     /*var base = prompt("Digite o valor da base do retângulo");
+        var altura = prompt("Digite o valor da altura do retângulo");
+        document.write(base * altura);*/
+
+        //Quadrado//
+
+        /*var lado = parseFloat(prompt("Informe o lado do quadrado"));
+        document.write(lado * 2);*/
+
+        //Losango//
+
+        /*var diagonal_maior = parseFloat(prompt("Digite a diagonal maior"));
+        var diagonal_menor = parseFloat(prompt("Digite a diagonal menor"));
+        document.write(diagonal_maior * diagonal_menor / 2);*/
+
+        //Paralelogramo//
+
+        /*var base = prompt("Digite a base do paralelogramo");
+        var altura = prompt("Digite a altura do paralelogramo");
+        document.write(base * altura);*/
+
+        //Triângulo//
+
+        /*var base = prompt("Digite a base do triângulo");
+        var altura = prompt("Digite a altura do triângulo");
+        document.write(base * altura / 2);*/
+
+        //Círculo//
+
+        /*var TT = promptparseFloat("Informe o número de Pi");
+        var R = promptparseFloat("Digite o valor de raio");
+        document.write(TT * (R * R));*/
